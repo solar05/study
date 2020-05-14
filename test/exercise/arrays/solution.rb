5b7f1d9440ddea69e6d35b8afcaa08b04e0b3168
solar05 <artem00298@gmail.com>
2020-05-14 22:16:44 +0300

    Implemented max element search, moved binary search to method

--- test/exercise/arrays/solution.rb
+++ test/exercise/arrays/solution.rb
@@ -2,26 +2,31 @@ module Exercise
   module Arrays
     class << self
       def replace(array)
-        max = array.max
+        max = find_max(array)
         array.map { |number| number.positive? ? max : number }
       end
 
+      def find_max(array)
+        array.inject { |max, elem| max > elem ? max : elem }
+      end
+
       def search(array, element)
-        def binary(array, left_border, right_border, element)
-          return -1 if left_border > right_border
-
-          middle = (left_border + right_border) / 2
-          return middle if element == array[middle]
-          return binary(array, left_border, middle - 1, element) if element < array[middle]
-
-          binary(array, middle + 1, right_border, element)
-        end
         length = array.length
         if length < 2
           return array.index(element).nil? ? -1 : array.index(element)
         end
 
         binary(array, 0, length, element)
+      end
+
+      def binary(array, left_border, right_border, element)
+        return -1 if left_border > right_border
+
+        middle = (left_border + right_border) / 2
+        return middle if element == array[middle]
+        return binary(array, left_border, middle - 1, element) if element < array[middle]
+
+        binary(array, middle + 1, right_border, element)
       end
     end
   end
